862a49f178ec84f44c3343621008fe44edf17ab4
Upinder Singh <upinderawat@gmail.com>
2022-01-17 13:12:20 +0530

    Added R&S operator tut

---
+++ tutorial11.ts
@@ -0,0 +1,16 @@
+let displayColors = function(msg, ...colors){
+    console.log(msg);
+    for(let c in colors){
+        console.log(colors[c]);
+    }
+};
+
+displayColors("one color", "Red");
+displayColors("Two colors", "Red", "Green");
+displayColors("Three colors", "Red", "Green", "Blue");
+let arrayOfColors = ["Red", "Green", "Blue", "White", "Black"];
+
+
+console.log("use of Spread Operator")
+
+displayColors("froma array", ...arrayOfColors); 
