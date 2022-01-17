f1b61e0f9588a874c53dbb3e9ff2ddab1e9ba6cb
Upinder Singh <upinderawat@gmail.com>
2022-01-17 21:58:09 +0530

    String Templates

---
+++ src/tutorial19.js
@@ -0,0 +1,10 @@
+//String Templates
+var main = function () {
+    var name = "Upinder";
+    var myString = "Welcome to the course " + name;
+    var easyString = "Welcome to 'exciting' and \"boring\" course, " + name + " \nand it even \n            SUPPORTS mutliline"; //NOTE: whitespaces are not trimmed/ignored
+    console.log(myString);
+    console.log(easyString);
+};
+main();
+//# sourceMappingURL=tutorial19.js.map
