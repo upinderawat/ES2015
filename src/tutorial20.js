9517f40addbda10e7bf5838549f1550bc056e5cd
Upinder Singh <upinderawat@gmail.com>
2022-01-17 22:39:28 +0530

    For of loop

---
+++ src/tutorial20.js
@@ -0,0 +1,10 @@
+var for1 = function () {
+    var colors = ["Red", "Greeen", "Yellow"];
+    for (var _i = 0, colors_1 = colors; _i < colors_1.length; _i++) {
+        var iterator = colors_1[_i];
+        console.log(iterator);
+    }
+    var letters = "abcdefghijklmnopqrstuvwxyz";
+};
+for1();
+//# sourceMappingURL=tutorial20.js.map
---
+++ tutorial20.ts
@@ -0,0 +1,9 @@
+let for1 = function(){
+    let colors = ["Red", "Greeen", "Yellow"];
+    for (const iterator of colors) {
+        console.log(iterator);
+    }
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    
+};
+for1();
