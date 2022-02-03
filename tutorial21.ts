0edfa8ba50066849eaa619c693eb41bfd0a05417
Upinder Singh <upinderawat@gmail.com>
2022-02-03 15:07:42 +0530

    Classes & Inheritence

---
+++ tutorial21.ts
@@ -0,0 +1,32 @@
+class Person{
+    //constructor
+    name:"";
+    constructor(name){
+        this.name = name;
+    }
+    static staticMethod(){
+        console.log("Hey its static method");
+    }
+    helloMe(){
+        console.log("hello it's "+ this.name + " from Person class");
+    }
+}
+class Employee extends Person{
+    id: "";
+    constructor(name, id){
+        super(name);
+        this.id = id;
+    }
+    getId(){
+        console.log("ID: "+ this.id+ " from Employee class"); 
+    }
+}
+let p = new Person("Upinder");
+let e = new Employee("Rohit", 2);
+
+e.helloMe();
+e.getId();
+// console.log(typeof Person);
+Person.staticMethod();
+p.helloMe();
+//Class is basically a function which is not hoisted
